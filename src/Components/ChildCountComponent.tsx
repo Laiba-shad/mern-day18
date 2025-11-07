41a4efcea1827dd563738baa0f5a65024b9a4d7a
Laiba Irshad <laibashad71@gmail.com>
2025-11-07 09:10:57 -0800

    Added new task

--- src/Components/ChildCountComponent.tsx
+++ src/Components/ChildCountComponent.tsx
@@ -2,18 +2,32 @@ import React, { useState } from "react";
 
 interface ChildComponentProps {
   initial: number;
-  OnIncrement: (value: number) => void;
+  parentCount: number;
+  onIncrement: (value: number) => void;
 }
 
 const ChildCountComponent = ({
   initial,
-  OnIncrement: ChildIncrement,
+  parentCount,
+  onIncrement,
 }: ChildComponentProps) => {
-  const [count, setCount] = useState(initial);
+  const [incre, setIncre] = useState(initial);
+
+  function handleIncrement() {
+    setIncre((prev) => prev + 1);
+    onIncrement(1);
+  }
 
   return (
-    <div className="bg-red-300 w-30 h-30 justify-center items-center">
-      <button onClick={() => ChildIncrement(initial)}>Increase</button>
+    <div className="bg-gray-300 p-4 rounded-xl flex flex-col items-center shadow-md">
+      <p className="font-semibold">Child Count: {incre}</p>
+      <p className="text-sm text-gray-700">Parent Count: {parentCount}</p>
+      <button
+        onClick={handleIncrement}
+        className="mt-2 bg-green-400 text-white px-3 py-1 rounded-md hover:bg-teal-700 transition"
+      >
+        + Increase
+      </button>
     </div>
   );
 };
--- src/Components/ParentCountComponent.tsx
+++ src/Components/ParentCountComponent.tsx
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
 import ChildCountComponent from "./ChildCountComponent";
+
 interface ParentCountComponentProps {
   init: number;
 }
 
 const ParentCountComponent = ({ init }: ParentCountComponentProps) => {
-  const [count, setCount] = useState(init);
+  const [count, setCount] = useState(0);
+
   function handleIncrement() {
     setCount((prev) => prev + 1);
   }
+
   return (
-    <div>
-      <h3>Increment {count}</h3>
-      <div className="flex gap-4 mt-4">
-        <ChildCountComponent initial={init} OnIncrement={handleIncrement} />
-        <ChildCountComponent initial={init} OnIncrement={handleIncrement} />
-        <ChildCountComponent initial={init} OnIncrement={handleIncrement} />
+    <div className="p-6">
+      <h3 className="text-xl font-bold mb-4">Parent Count: {count}</h3>
+
+      <div className="flex gap-4 flex-wrap">
+        <ChildCountComponent
+          initial={init}
+          parentCount={count}
+          onIncrement={handleIncrement}
+        />
+        <ChildCountComponent
+          initial={init}
+          parentCount={count}
+          onIncrement={handleIncrement}
+        />
+        <ChildCountComponent
+          initial={init}
+          parentCount={count}
+          onIncrement={handleIncrement}
+        />
       </div>
     </div>
   );
